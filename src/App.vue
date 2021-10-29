fa12dca4b8edb5aaa293e764d523af22d2edb501
azur1s <73991665+azur1s@users.noreply.github.com>
2021-10-29 14:11:21 +0700

    replaced placeholders

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/wafuwafu.gif" />
+  <Main msg="Welcome" />
 </template>
 
 <style>
+
+@import url('http://fonts.cdnfonts.com/css/helvetica-neue-9');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+body { 
+  background-color: #212529;
 }
 </style>
