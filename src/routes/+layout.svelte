b6340b6a1aa86085f032c705a95837a97aa5e85c
azur <natapat.samutpong@gmail.com>
2023-03-12 06:41:51 +0700

    change icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 <div class="ml-24rem mr-24rem h-screen">
     <nav class="py-1rem flex">
         <div class="flex-1 justify-start">
-            <a class="text-light-900 no-underline" href="/">A</a>
+            <a class="text-light-900 no-underline" href="/">azur.moe</a>
         </div>
         <div class="flex-1 gap-x-2rem inline-flex justify-end">
             <NavLink to={'/'}>/home</NavLink>
