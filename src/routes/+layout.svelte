ec8a5e815591783c52484da3fc44a85899867c86
azur <natapat.samutpong@gmail.com>
2024-12-20 11:34:43 +0700

    hi

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,176 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+import "../app.css";
+
+const scrollToEl = (event: { target: EventTarget | null }) => {
+    const target = event.target as HTMLAnchorElement | null;
+    const el = document.querySelector(target!.getAttribute("href")!);
+    if (!el) return;
+    // el.scrollIntoView({ behavior: "smooth" });
+    const offset = 64 + 28;
+    const y = el.getBoundingClientRect().top + window.scrollY - offset;
+    window.scrollTo({ top: y, behavior: "smooth" });
+};
+
+onMount(() => {
+    const scrollToTop = document.getElementById("scrollToTop");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 0) {
+            scrollToTop?.classList.remove("opacity-0");
+            scrollToTop?.classList.add("opacity-100");
+        } else {
+            scrollToTop?.classList.remove("opacity-100");
+            scrollToTop?.classList.add("opacity-0");
+        }
+    });
+});
+</script>
+
+<main id="root" class="flex flex-col">
+    <div class="fixed bottom-4 right-4 z-10">
+        <button
+            id="scrollToTop"
+            class="p-4 m-4 backdrop-invert rounded-full shadow-md opacity-0 transition-opacity duration-300 ease-in-out"
+            on:click|preventDefault={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            aria-label="Go to top"
+        >
+            <svg
+                class="w-6 h-6 text-mono-100"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 10l7-7m0 0l7 7m-7-7v18"
+                ></path>
+            </svg>
+        </button>
+    </div>
+
+    <header class="m-auto mb-0 min-w-full sticky z-10 top-0 bg-mono-100">
+        <nav class="flex items-center justify-between py-8 px-12 text-lg">
+            <a class="link" on:click|preventDefault={scrollToEl} href="#root">azur</a>
+            <div class="flex items-center space-x-4">
+                <a class="link" on:click|preventDefault={scrollToEl} href="#discography">discography</a>
+                <a class="link" on:click|preventDefault={scrollToEl} href="#gallery">gallery</a>
+                <a class="link" on:click|preventDefault={scrollToEl} href="#about">about</a>
+            </div>
+        </nav>
+    </header>
+
+    <div class="flex flex-col flex-grow h-screen-no-header">
+        <div class="
+        min-w-full h-full p-0
+        flex flex-row flex-grow
+    ">
+        <div class="flex-col box flex-[2]">
+            <div class="flex-row box flex-[2] hover:flex-[2.25]">
+                <a
+                    class="
+                        box flex-1 flex-col justify-end hover:flex-[1.25]
+                        imgbg bg-[url('images/latest.png')] group
+                    "
+                    href="#discography"
+                    on:click|preventDefault={scrollToEl}
+                    data-width="1280"
+                    data-height="720"
+                >
+                    <div class="p-8 bg-shadow">
+                        <p class="text-xl font-medium">Discography</p>
+                        <p class="text-sm">See all releases</p>
+                    </div>
+                </a>
+                <a
+                    class="
+                        box flex-1 flex-col justify-end hover:flex-[1.25]
+                        imgbg bg-[url('images/wide2.jpg')] group
+                    "
+                    href="#gallery"
+                    on:click|preventDefault={scrollToEl}
+                    data-width="4096"
+                    data-height="1714"
+                >
+                    <div class="p-8 bg-shadow">
+                        <p class="text-xl font-medium">Gallery</p>
+                        <p class="text-sm">View artworks</p>
+                    </div>
+                </a>
+            </div>
+
+            <a
+                class="
+                    box flex-1 flex-col justify-end hover:flex-[1.25]
+                    imgbg bg-[url('images/wide.jpg')] group
+                "
+                href="#about"
+                on:click|preventDefault={scrollToEl}
+                data-width="2560"
+                data-height="1080"
+            >
+                <div class="p-8 bg-shadow">
+                    <p class="text-xl font-medium">About</p>
+                    <p class="text-sm">Learn more</p>
+                </div>
+            </a>
+        </div>
+    </div>
+    </div>
+
+    <div class="prose prose-mono prose-invert p-10">
+        <div class="mb-[500px]" id="discography">
+            <h1>Discography</h1>
+            <p>todo</p>
+        </div>
+
+        <div class="mb-[500px]" id="gallery">
+            <h1>Gallery</h1>
+            <p>todo</p>
+        </div>
+
+        <div class="mb-[500px]" id="about">
+            <h1>About</h1>
+            <p>hi i make music</p>
+        </div>
+    </div>
+</main>
+
+<style lang="postcss">
+:global(html) {
+    background-color: theme(colors.mono.100);
+    color: theme(colors.mono.900);
+    font-family: "Geist", sans-serif;
+}
+
+.h-screen-no-header {
+    height: calc(100vh - 64px - 28px);
+}
+
+.link {
+    color: theme(colors.mono.900);
+    text-decoration: none;
+    transition: color 0.2s;
+}
+
+.link:hover {
+    color: theme(colors.blue.600);
+}
+
+.box {
+    @apply flex transition-[flex-grow] ease-in-out duration-500;
+}
+
+.imgbg {
+    @apply bg-no-repeat bg-center bg-cover;
+}
+
+.bg-shadow {
+    @apply
+        bg-gradient-to-t from-mono-100 to-[#00000000]
+        opacity-25 group-hover:opacity-100
+        transition-all duration-300 ease-in-out;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,77 +1,54 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
+    colors: {
+      'mono': {
+        100: '#000000',
+        200: '#161616',
+        300: '#262626',
+        400: '#393939',
+        500: '#525252',
+        700: '#dde1e6',
+        800: '#f2f4f8',
+        900: '#FFFFFF',
       },
+      'pink': {
+        100: '#2A0A18',
+        200: '#510224',
+        300: '#740937',
+        400: '#9F1852',
+        500: '#D02670',
+        600: '#EE5396',
+        700: '#FF7EB6',
+        800: '#FFAFD2',
+        900: '#FED7E8',
+      },
+      'blue': {
+        100: '#001141',
+        200: '#001D6C',
+        300: '#012D9C',
+        400: '#0043CE',
+        500: '#0E62FE',
+        600: '#4589FF',
+        700: '#77A9FF',
+        800: '#A6C8FF',
+        900: '#D0E2FF',
+      }
     },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      typography: ({ theme }) => ({
+        mono: {
+          css: {
+            '--tw-prose-body': theme('colors.mono[900]'),
+            '--tw-prose-headings': theme('colors.mono[900]'),
+          }
+        }
+      }),
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
+
