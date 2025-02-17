0d3de11ef19fceb78ad77bbe128dbf5b6769e65d
azur <natapat.samutpong@gmail.com>
2025-02-18 03:36:39 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,103 @@
 <script lang="ts">
 import "../app.css";
 import Logo from "../components/Logo.svelte";
-import D from "../components/d.svelte";
-
 </script>
 
-<div class="flex flex-col flex-grow items-center justify-center h-full gap-4">
-    <div class="text-mono-900 max-sm:w-[45%] w-[18rem] h-auto">
+<div class="prose prose-mono mx-auto py-16">
+    <!-- <div class="text-mono-900 max-sm:w-[45%] w-[18rem] h-auto">
+        <Logo />
+    </div> -->
+
+    <div class="not-prose text-mono-900 max-sm:w-[45%] w-[18rem] h-auto">
         <Logo />
     </div>
 
-    <div
-        class="text-pink-600 text-xl"
-    >
-        azur1s
+    <p class="mx-auto text-blue-500">───────────┤ azur1s ├───────────</p>
+
+    <p>
+        hi, i'm azur1s. i make music bla bla bla. bro i don't that famous
+        to be putting shit here xd.<br/><br/>
+        my website is under construction. so don't expect much here :P<br/>
+        contact me on instagram or discord if you have something :)
+    </p>
+
+    <p class="whitespace-pre text-mono-500">
+˚　　　　✦　　　.　　. 　 ˚　.　　　　　 . ✦　　　 　˚　　　　 . ★  ⋆.
+　　　.   　　˚　　 　　*　　 　　✦　　　.　　.　　　✦　˚ 　　　　 ˚　.˚
+    </p>
+
+    <img
+        src="/images/banner_choco.png"
+        alt="Banner"
+        class="w-full h-auto rounded-lg"
+    />
+    <p class="text-mono-500">
+        └ Logo commissioned from
+        <a
+            href="https://x.com/chocographix"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-blue-300 hover:text-blue-500 transition-colors"
+        >
+            chocographix
+        </a>
+    </p>
+
+    <p class="mx-auto text-blue-500">─────────┤ discography ├─────────</p>
+    <p class="mb-32">WIP - check out 
+        <a
+            href="https://www.youtube.com/channel/UCLhvVj-vd0pkfqxJVyga7Sw/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-blue-500"
+        >
+            youtube
+        </a>
+    </p>
+
+    <p class="mx-auto text-blue-500">───────────┤ gallery ├───────────</p>
+    <p class="mb-32">WIP</p>
+
+    <p class="mx-auto text-blue-500">───────────┤ contact ├───────────</p>
+    <div class="whitespace-pre">
+Instagram     azur1s             (fastest, recommended)
+Discord       azur1s
+Mail          contact@azur.moe
     </div>
 
-    <div class="flex flex-col gap-1 items-center">
-        <a
-            class="linke"
-            href="/discography"
-        >discography</a>
-        <a
-            class="linke"
-            href="/gallery"
-        >gallery</a>
-        <a
-            class="linke"
-            href="/contact"
-        >contact</a>
-        <a
-            class="linke"
-            href="/about"
-        >about</a>
-        <D />
-    </div>
+    <p class="whitespace-pre text-mono-500">
+                 '
+            *          .
+                   *       '
+              *                *
+
+
+
+
+
+   *   '*
+           *
+                *
+                       *
+               *
+                     *
+
+               .                      .
+               .                      ;
+               :                  - --+- -
+               !           .          !
+               |        .             .
+               |_         +
+            ,  | `.
+- --  --- --+-&lt;#>-+- ---  --  -
+            `._|_,'
+               T
+               |
+               !
+               :         . : 
+               .       *
+    </p>
 </div>
 
 <style lang="postcss">
-.linke {
-    @apply
-        text-mono-900 hover:text-pink-400
-        focus:text-pink-400 focus:outline-none
-        transition-colors duration-200
-}
 </style>
